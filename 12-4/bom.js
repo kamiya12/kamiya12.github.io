ab3a701770f8ee315a0444505399b550fd2f931e
kamiya12 <2807186629@qq.com>
2016-05-18 12:39:39 +0800

    xin

--- 12-4/bom.js
+++ 12-4/bom.js
@@ -9,5 +9,37 @@ var person = {
 	}
 }
 
-console.log(person.name);
-person.sing();
+//console.log(person.name);
+//person.sing();
+// for (var i in person)(
+//      console.log(i + ":" + person[i]);
+//)
+
+
+
+
+for(var i in console){
+	console.log(i + ":" + console[i]);
+}
+console.clear();
+
+console.log(typeof person);
+
+// 布尔类型
+
+console.log(typeof false);
+console.log(typeof 'false');
+
+// 数字类型
+console.log(typeof 100);
+ 
+// 字符串类型
+console.log(typeof person.name);
+
+
+// 定义数组
+var sites = ['baidui.com','sohu.com','sina.com','163.com'];
+
+console.log(sites[0]);
+ 
+console.log(typeof sites);
